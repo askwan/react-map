46154c1fd97d73cc8c41ad2e5a7832e1ddfd1310
infiniteWang <631951201@qq.com>
2019-04-04 11:32:10 +0800

    1

--- src/script/map/index.js
+++ src/script/map/index.js
@@ -8,8 +8,6 @@ import DrawCircleTool from './tool/DrawCircleTool'
 import DrawRectangleTool from './tool/DrawRectangleTool'
 import Wkt from 'wicket'
 
-import * as turf from '@turf/turf'
-
 mapboxgl.accessToken = 'pk.eyJ1IjoieHRwZ2t4ayIsImEiOiJSUEhldmhZIn0.dJzz5bXztrZRAViAdmQvyQ';
 
 let map;
